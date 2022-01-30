6345d54ecba6b5431d18909665bfa7fa201e3b85
MustafaShami <mustafa.shami@ucdenver.edu>
2022-01-30 15:51:54 -0700

    Instead of passing back rawdata we created new object and then returned that.

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -8,7 +8,17 @@ module.exports = async (phrase) => {
         }
     })
 
-    return JSON.stringify(results.data);
+    //return JSON.stringify(results.data);
+    // Instead of passing back the raw data(above) we are going to create a new object (below)
+    let customObject = {
+        data: results.data,
+            status: results.status,
+            statusTest: results.statusText,
+            headers: results.headers,
+            requestHeader: results.config.headers
+    }
+
+    return customObject;
 }
 
 /*  console.log(results.data);
